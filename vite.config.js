09da129149b8c3ba21743bac9478b8c6739586ed
Marcos <markbiophysicist@gmail.com>
2026-07-01 20:23:14 +0200

    chore: initial project setup

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { vitePlugin as remix } from "@remix-run/dev";
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+export default defineConfig({
+  plugins: [
+    tailwindcss(),
+    remix({
+      ignoredRouteFiles: ["**/*.css"],
+    }),
+    tsconfigPaths(),
+  ],
+});
